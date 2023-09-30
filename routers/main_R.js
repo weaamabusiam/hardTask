c8e4c0f86417e5d3ec909f1125a6f237a516127a
weaamabusiam <weam.abo99@gmail.com>
2023-09-30 18:41:49 +0300

    הוספת תאריך

--- routers/main_R.js
+++ routers/main_R.js
@@ -11,6 +11,8 @@ router.get("/", function (req, res) {
     res.render("main", {});
 });
 
+// ... כאן נשאר הקוד הקודם שלך
+
 function saveEntry(employeeId, name, id, date) {
     const formattedTime = format(new Date(), "yyyy-MM-dd HH:mm:ss");
     const query = `INSERT INTO entry_exit (employee_id, name, id, date, entry_time) VALUES (${employeeId}, '${name}', '${id}', '${date}', '${formattedTime}')`;
@@ -37,130 +39,29 @@ function saveExit(employeeId) {
     });
 }
 
-router.get('/entry-exit', function (req, res) {
-    const selectQuery = 'SELECT * FROM employees';
+router.post('/clockIn', function (req, res) {
+    const { name, id } = req.body;
+    const date = new Date().toLocaleDateString();
 
-    db_pool.query(selectQuery, function (err, employees, fields) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.render('entry-exit', { employees });
-        }
-    });
+    // שמור את הכניסה במסד הנתונים
+    saveEntry(id, name, id, date);
+
+    // עבור הדוגמה, אני משתמש בזמן רנדומלי כזמן הכניסה
+    const randomTime = format(new Date(), "HH:mm:ss");
+
+    // אתה יכול לשנות את randomTime לזמן המתקבל מהשרת או לשימוש בזמן אמיתי אחר
+    res.json({ date: date, exactTime: randomTime });
 });
 
-router.get('/employees-management', function (req, res) {
-    const selectQuery = 'SELECT * FROM employees';
+router.post('/clockOut', function (req, res) {
+    const { name, id } = req.body;
 
-    db_pool.query(selectQuery, function (err, employees, fields) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.render('employees-management', { employees });
-        }
-    });
+    // שמור את היציאה במסד הנתונים
+    saveExit(id);
+
+    // עבור הדוגמה, אני משתמש בזמן רנדומלי כזמן היציאה
+    const randomTime = format(new Date(), "HH:mm:ss");
+
+    // אתה יכול לשנות את randomTime לזמן המתקבל מהשרת או לשימוש בזמן אמיתי אחר
+    res.json({ exactTime: randomTime });
 });
-
-router.post('/employees-management/add', function (req, res) {
-    const { first_name, last_name } = req.body;
-    const insertQuery = `INSERT INTO employees (first_name, last_name) VALUES ('${first_name}', '${last_name}')`;
-
-    db_pool.query(insertQuery, function (err, result) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.redirect('/employees-management');
-        }
-    });
-});
-
-router.post('/employees-management/update', function (req, res) {
-    const { employee_id, first_name, last_name } = req.body;
-    const updateQuery = `UPDATE employees SET first_name = '${first_name}', last_name = '${last_name}' WHERE employee_id = ${employee_id}`;
-
-    db_pool.query(updateQuery, function (err, result) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.redirect('/employees-management');
-        }
-    });
-});
-
-router.post('/employees-management/delete', function (req, res) {
-    const { employee_id } = req.body;
-    const deleteQuery = `DELETE FROM employees WHERE employee_id = ${employee_id}`;
-
-    db_pool.query(deleteQuery, function (err, result) {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.redirect('/employees-management');
-        }
-    });
-});
-
-router.get("/getEmployees", (req, res) => {
-    const query = "SELECT * FROM employees";
-    db_pool.query(query, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: "שגיאה בשאלה למסד נתונים" });
-        } else {
-            res.json(results);
-        }
-    });
-});
-
-router.post("/addEmployee", (req, res) => {
-    const { name, id, date, entry, exit } = req.body;
-    const query = `INSERT INTO employees (name, id, date, entry, exit) VALUES (?, ?, ?, ?, ?)`;
-    const values = [name, id, date, entry, exit];
-
-    db_pool.query(query, values, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: "שגיאה בהוספת העובד למסד נתונים" });
-        } else {
-            res.json({ message: "העובד נוסף בהצלחה" });
-        }
-    });
-});
-
-router.put("/updateEmployee/:id", (req, res) => {
-    const id = req.params.id;
-    const { name, date, entry, exit } = req.body;
-    const query = `UPDATE employees SET name = ?, date = ?, entry = ?, exit = ? WHERE id = ?`;
-    const values = [name, date, entry, exit, id];
-
-    db_pool.query(query, values, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: "שגיאה בעדכון פרטי העובד במסד נתונים" });
-        } else {
-            res.json({ message: "פרטי העובד עודכנו בהצלחה" });
-        }
-    });
-});
-
-router.delete("/deleteEmployee/:id", (req, res) => {
-    const id = req.params.id;
-    const query = "DELETE FROM employees WHERE id = ?";
-    const values = [id];
-
-    db_pool.query(query, values, (err, results) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: "שגיאה במחיקת העובד ממסד הנתונים" });
-        } else {
-            res.json({ message: "העובד נמחק בהצלחה" });
-        }
-    });
-});
-
-module.exports = router;
